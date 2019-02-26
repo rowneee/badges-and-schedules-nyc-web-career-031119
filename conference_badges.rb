1060255d339f2d4c0810423972019e5ffa289e35
Roni Shabo <roni.shabo@gmail.com>
2019-02-26 15:39:46 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  names.collect do |name|
+  badge_maker(name)
+  end
+end
+
+def assign_rooms(speakers)
+  rooms = []
+  speakers.each_with_index do |speaker, room|
+  rooms << "Hello, #{speaker}! You'll be assigned to room #{room+1}!"
+  end
+  return rooms
+end
+
+def printer
+  batch_badge_creator
+  assign_rooms
+end
